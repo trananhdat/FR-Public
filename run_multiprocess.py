234c523ae2fc8ac33588b96a2f9edbaa7f18efa3
ngoctuhan <48152937+ngoctuhan@users.noreply.github.com>
2021-03-06 17:25:46 +0700

    Update run_multiprocess.py

--- run_multiprocess.py
+++ run_multiprocess.py
@@ -2,10 +2,20 @@ import argparse
 import tkinter as tk
 from frame import AppFrame
 from frame_sleep import SleepFrame
+"""
+10◇May @tw1tt3rart
+┈┈┈┈◢▇▇▇▇▇◣┈┈┈┈┈
+┈┈┈┈▉◤◤◤◤◤▊▍#NgocTuHan
+┈┈▕▔▉┏▅╮╭▅┓▍▏┈┈┈
+┈┈┈╲◥╲▔▕▕▔▕╱┈┈┈┈
+┈┈┈┈┈▍▔╱▔╲▕┈┈┈┈┈
+┈┈┈┈▇◣╲▔▔▕◢▍┈┈┈┈
+┈◢▇▇▊▊▕▇▇▏▉▇▇◣┈┈
+"""
 
 ap = argparse.ArgumentParser()
 
-ap.add_argument("--video_src", type=int, default= 1,help="Source camera")
+ap.add_argument("--video_src", type=int, default= 3,help="Source camera")
 args = ap.parse_args()
    
 root = tk.Tk()
