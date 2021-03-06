82806c76384c00e94dbd80f5838bb3d074a27061
ngoctuhan <hoangmautrung@gmail.com>
2021-03-06 17:24:28 +0700

    fix name file

--- run_multiprocess.py
+++ run_multiprocess.py
@@ -1,21 +1,11 @@
 import argparse
 import tkinter as tk
-from main_frame import AppFrame
+from frame import AppFrame
 from frame_sleep import SleepFrame
-"""
-10◇May @tw1tt3rart
-┈┈┈┈◢▇▇▇▇▇◣┈┈┈┈┈
-┈┈┈┈▉◤◤◤◤◤▊▍#NgocTuHan
-┈┈▕▔▉┏▅╮╭▅┓▍▏┈┈┈
-┈┈┈╲◥╲▔▕▕▔▕╱┈┈┈┈
-┈┈┈┈┈▍▔╱▔╲▕┈┈┈┈┈
-┈┈┈┈▇◣╲▔▔▕◢▍┈┈┈┈
-┈◢▇▇▊▊▕▇▇▏▉▇▇◣┈┈
-"""
 
 ap = argparse.ArgumentParser()
 
-ap.add_argument("--video_src", type=int, default= 3,help="Source camera")
+ap.add_argument("--video_src", type=int, default= 1,help="Source camera")
 args = ap.parse_args()
    
 root = tk.Tk()
